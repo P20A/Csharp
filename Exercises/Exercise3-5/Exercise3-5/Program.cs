48da30fc05a1740397c63afa05edd83cf99ee75d
unknown <parsa20ataei@gmail.com>
2025-11-16 10:43:48 +0330

    adding Exercise3-5

---
+++ Exercises/Exercise3-5/Exercise3-5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your number:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double result;
+            if (x < 0)
+            {
+                result = x + 1;
+            }
+            else if (x >= 0 && x < 1)
+            {
+                result = -(Math.Pow(x, 2));
+            }
+            else 
+            {
+                result = 1d / x;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
