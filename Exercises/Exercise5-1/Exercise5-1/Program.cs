3c3a4ade46545fa84b14aa753bdcf69304ec3cd2
unknown <parsa20ataei@gmail.com>
2025-11-25 21:53:11 +0330

    adding exercises 5-1, 5-2

---
+++ Exercises/Exercise5-1/Exercise5-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your 2 based number:");
+            int x = int.Parse(Console.ReadLine());
+            int temp = x;
+            double result = 0;
+            int counter = 0;
+            while (x != 0)
+            {
+                if (x % 2 == 1)
+                {
+                    result += Math.Pow(2, counter);
+                }
+                x /= 10;
+                counter++;
+            }
+            Console.WriteLine($"{temp} ==> {result}");
+        }
+    }
+}
---
+++ Exercises/Exercise5-2/Exercise5-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your 8-bit number:");
+            int x = int.Parse(Console.ReadLine());
+            int temp = x;
+            string result = "";
+            for (int i = 7; i >= 0; i--)
+            {
+                if (x >= Math.Pow(2, i))
+                {
+                    x -= (int)Math.Pow(2, i);
+                    result += "1";
+                }
+                else
+                {
+                    result += "0";
+                }
+            }
+            Console.WriteLine($"{temp} ==> {result}");
+        }
+    }
+}
