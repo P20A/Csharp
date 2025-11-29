e8cda8992072a1255d49bee32e96c26ea63f9bdd
unknown <parsa20ataei@gmail.com>
2025-11-29 12:37:55 +0330

    added Exercises 5-4, 5-5, 5-6 and edited imgs of Exercises 4-6, 4-7

---
+++ Exercises/Exercise5-5/Exercise5-5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the limit: ");
+            int limit = int.Parse(Console.ReadLine());
+            bool valid = false;
+            int sum = 0;
+            for (int i = 2 ; i < limit; i++)
+            {
+                valid = true;
+                for (int j = 2; j < limit; j++)
+                {
+                    if (i == 2 || i == 3 || i == 5 || i == 7)
+                        break;
+                    if (i != j && i % j == 0)
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Exercises/Exercise5-6/Exercise5-6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the n: ");
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                    counter++;
+                }
+                else
+                {
+                    n = (n * 3) + 1;
+                    counter++;
+                }   
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
