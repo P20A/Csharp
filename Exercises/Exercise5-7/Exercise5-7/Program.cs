50a173f9f548a87bb2a1168298c6ebf0b25ebe33
parsa <parsa20ataei@gmail.com>
2025-11-29 22:04:12 +0330

    added Exercises 5-7, 6-1

---
+++ Exercises/Exercise5-7/Exercise5-7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the eqoaition is: a*x + b*y = c");
+            Console.Write("enter a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("enter b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("enter c: ");
+            int c = int.Parse(Console.ReadLine());
+            int eqo = 0;
+            for (int x = 0; x <= c; x++)
+            {
+                for (int y = 0; y <= c; y++)
+                {
+                    eqo = a * x + b * y;
+                    if (eqo == c)
+                    {
+                        Console.WriteLine($"{a}*{x} + {b}*{y} = {c}");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Exercises/Exercise6-1/Exercise6-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"enter #{i + 1} number: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int j = array.Length-1;
+            int[] reverse = new int[10];
+            for (int i = 0;i < array.Length; i++)
+            {
+                reverse[i] = array[j];
+                j--;
+            }
+            foreach (int i in reverse)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}   
