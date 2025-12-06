968e3775c1acf1733c2771116546aad18386622e
parsa <parsa20ataei@gmail.com>
2025-12-06 23:15:35 +0330

    added Exercise 6-12

---
+++ Exercises/Exercise6-12/Exercise6-12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the lenth of your array: ");
+            int[] array = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"enter the #{i + 1} number: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            // اولین بزرگ ترین پاک می شود:
+            biggerFinder(array);
+            // دومین بزرگ ترین نمایش داده میشود:
+            Console.WriteLine(biggerFinder(array));
+
+        }
+        static int biggerFinder(int[] array)
+        {
+            int result = 0;
+            bool valid = false;
+            int validCounter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == int.MinValue)
+                    continue;
+                validCounter = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] > array[j] && j != i)
+                    {
+                        validCounter++;
+                    }
+                    if (validCounter == array.Length - 1)
+                    {
+                        valid = true;
+                        result = array[i];
+                        array[i] = int.MinValue;
+                        break;
+                    }
+                }
+                if (valid)
+                    break;
+            }
+            return result;
+        }
+    }
+}
