365f9a975ee29344b75c9e247cb60a0d8134c750
parsa <parsa20ataei@gmail.com>
2025-12-08 22:34:14 +0330

    added Exercises 6-10, 6-11

---
+++ Exercises/Exercise6-10/Exercise6-10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the length of your array: ");
+            int[] array = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"enter the #{i+1} number: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("\n\nenter the number u want to inject: ");
+            int newNum = int.Parse(Console.ReadLine());
+            int[] newArray = new int[array.Length+1];
+            for (int i = 0; i < newArray.Length-1; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[newArray.Length-1] = newNum;
+
+            foreach (int i in newArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Exercises/Exercise6-11/Exercise6-11/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the length of your array: ");
+            int[] array = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"enter the #{i + 1} number: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("\n\nenter the number u want to remove: ");
+            int numToRemove = int.Parse(Console.ReadLine());
+            int[] newArray = new int[array.Length - 1];
+            int placeholder = 0;
+            bool error = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == numToRemove)
+                {
+                    placeholder = i;
+                    break;
+                }
+                else if (newArray.Length != i)
+                    newArray[i] = array[i];
+                else
+                {
+                    Console.WriteLine("the number u wanted to remove doenst exist in the array.");
+                    error = true;
+                    break;
+                }
+            }
+            for (int i = placeholder; i < newArray.Length && !error; i++)
+                newArray[i] = array[i + 1];
+            if (!error)
+                foreach (int i in newArray)
+                    Console.WriteLine(i);
+        }
+    }
+}
