4dd9fff0ab015de3fff3c2ccab97336824d7f15b
parsa <parsa20ataei@gmail.com>
2025-12-13 23:24:37 +0330

    added Exercises 7-1,7-2

---
+++ Exercises/Exercise7-1/Exercise7-1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Exercise7_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the length of the first array: ");
+            int[] array1 = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write($"enter the #{i + 1} number: ");
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("enter the length of the first array: ");
+            int[] array2 = new int[int.Parse(Console.ReadLine())];
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write($"enter the #{i + 1} number: ");
+                array2[i] = int.Parse(Console.ReadLine());
+            }
+            int counter = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            int[] array3 = new int[counter];
+            counter = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                    {
+                        array3[counter] = array1[i];
+                        counter++;
+                    }
+                }
+            }
+            foreach (var item in array3)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Exercises/Exercise7-2/Exercise7-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the amount of students: ");
+            int[,] students = new int[int.Parse(Console.ReadLine()),2];
+            for (int i = 0; i < students.GetLength(0); i++)
+            {
+                Console.Write($"enter the #{i+1} student's id: ");
+                students[i,0] = int.Parse(Console.ReadLine());
+                Console.Write($"enter the #{i + 1} student's study code: ");
+                students[i, 1] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < students.GetLength(0); i++)
+            {
+                if (students[i, 1] == int.MaxValue) { continue; }
+                Console.WriteLine($"--------------{students[i, 1]}--------------");
+                Console.WriteLine(students[i, 0]);
+                for (int j = i+1; j < students.GetLength(0); j++)
+                {
+                    if (students[i, 1] == students[j, 1])
+                    {
+                        students[j, 1] = int.MaxValue;
+                        Console.WriteLine(students[j,0]);
+                    }
+                }
+                
+            }
+        }
+    }
+}
