b13e13fd1edb67719557999cd84114f29ca9eb31
parsa <parsa20ataei@gmail.com>
2025-12-20 18:10:46 +0330

    added Exercise9-1

---
+++ Exercises/Exercise9-1/Exercise9-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            checker(2, 5, 5, 76, 74);
+        }
+        static void checker(params int[] numbers)
+        {
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    a = numbers[i];
+                    b = numbers[j];
+
+                    if (a != 0 && b != 0)
+                    {
+                        if (a % b == 0 || b % a == 0)
+                        {
+                            Console.WriteLine($"{a} , {b}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
