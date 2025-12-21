6ac64e274561e1b945b0d18c106a54795e7380fd
unknown <parsa20ataei@gmail.com>
2025-12-21 20:29:22 +0330

    edited Exercises 9-7, 9-9

---
+++ Exercises/Exercise9-7/Exercise9-7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter: ");
+            string value = Console.ReadLine();
+            int result;
+            bool valid = validator(value, out result);
+            if(valid)
+            {
+                Console.WriteLine($"the value is: {result}");
+            }
+            else
+            {
+                Console.WriteLine("the value can not be converted.");
+            }
+
+
+        }
+        static bool validator(string value, out int result)
+        {
+            result = 0;
+            try
+            {
+                result = int.Parse(value);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+                
+        }
+    }
+}
---
+++ Exercises/Exercise9-9/Exercise9-9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalcArea(4));
+            Console.WriteLine(CalcArea(5,6));
+            Console.WriteLine(CalcArea(8,9,0));
+        }
+        static int CalcArea(int radius)
+        {
+            return Convert.ToInt32(Math.PI * Math.Pow(radius,2));
+        }
+        static int CalcArea(int x,int y)
+        {
+            return x * y;
+        }
+        static int CalcArea(int Base, int height,int none)
+        {
+            return (Base * height) / 2;
+        }
+    }
+}
